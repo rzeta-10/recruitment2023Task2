851192049c0990c6a81523d8439b1c8c836f8888
camelotsb <dynamist.sb@gmail.com>
2023-06-01 16:22:58 +0530

    Create colorTh.py

---
+++ colorTh.py
@@ -0,0 +1,21 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt 
+import numpy as np
+import math
+
+image= 'file/path/image.jpg'
+
+#RGB(3 streams) matrix representation of the image
+labrat = mpimg.imread('')
+
+'''
+
+Write the script wrt the given instruction here.
+
+'''
+
+
+
+imgplot = plt.imshow(labrat)
+
+plt.show()
